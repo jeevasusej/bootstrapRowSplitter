68312bc69c2676e8d9cf0ca20cb34f290fb58786
jeevasusej <jeevasusej@gmail.com>
2017-02-25 00:43:32 +0530

    script error

--- sample/script.js
+++ sample/script.js
@@ -5,7 +5,7 @@ angular.module('demo').controller('demoController',['$scope',function($scope){
 	,{name:'Narcotics Anonymous', description:'Insanity is doing the same thing, over and over again, but expecting different results.'}
 	,{name:'Ralph Waldo Emerson', description:"Finish each day and be done with it. You have done what you could. Some blunders and absurdities no doubt crept in; forget them as soon as you can. Tomorrow is a new day. You shall begin it serenely and with too high a spirit to be encumbered with your old nonsense."}
 	,{name:'André Gide, Autumn Leaves', description:"It is better to be hated for what you are than to be loved for what you are not."}
-	,{name:'Albert Einstein', description:“There are only two ways to live your life. One is as though nothing is a miracle. The other is as though everything is a miracle."}
+	,{name:'Albert Einstein', description:"There are only two ways to live your life. One is as though nothing is a miracle. The other is as though everything is a miracle."}
 	,{name:'Mark Twain', description:"Good friends, good books, and a sleepy conscience: this is the ideal life."}
 	,{name:'Allen Saunders', description:"Life is what happens to us while we are making other plans."}
 	,{name:'Douglas Adams, The Long Dark Tea-Time of the Soul', description:'I may not have gone where I intended to go, but I think I have ended up where I needed to be.'}
