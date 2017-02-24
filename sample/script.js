0aee16728c7fef0eb6e8cbb71a363e9db8f6ebf4
jeevasusej <jeevasusej@gmail.com>
2017-02-25 00:35:16 +0530

    demo scripts with data

--- sample/script.js
+++ sample/script.js
@@ -1,1 +1,20 @@
-alert('script loaded');
+angular.module('demo',['jjBootstrapRowSplitter']);
+angular.module('demo').controller('demoController',['$scope',function($scope){
+	var data=[
+	{name:'Oscar Wilde', description:'To live is the rarest thing in the world. Most people exist, that is all.'}
+	,{name:'Narcotics Anonymous', description:'Insanity is doing the same thing, over and over again, but expecting different results.'}
+	,{name:'Ralph Waldo Emerson', description:"Finish each day and be done with it. You have done what you could. Some blunders and absurdities no doubt crept in; forget them as soon as you can. Tomorrow is a new day. You shall begin it serenely and with too high a spirit to be encumbered with your old nonsense."}
+	,{name:'André Gide, Autumn Leaves', description:"It is better to be hated for what you are than to be loved for what you are not."}
+	,{name:'Albert Einstein', description:“There are only two ways to live your life. One is as though nothing is a miracle. The other is as though everything is a miracle."}
+	,{name:'Mark Twain', description:"Good friends, good books, and a sleepy conscience: this is the ideal life."}
+	,{name:'Allen Saunders', description:"Life is what happens to us while we are making other plans."}
+	,{name:'Douglas Adams, The Long Dark Tea-Time of the Soul', description:'I may not have gone where I intended to go, but I think I have ended up where I needed to be.'}
+	,{name:'Markus Zusak, I Am the Messenger', description:"Sometimes people are beautiful. Not in looks. Not in what they say. Just in what they are."}
+	,{name:'George Bernard Shaw', description:"Life isn't about finding yourself. Life is about creating yourself."}
+	,{name:'Albert Einstein', description:"Life is like riding a bicycle. To keep your balance, you must keep moving."}
+	,{name:'Bill Watterson, The Complete Calvin and Hobbes', description:"Reality continues to ruin my life."}
+	,{name:'J.K. Rowling, Harry Potter and the Deathly Hallows', description:"Dumbledore watched her fly away, and as her silvery glow faded he turned back to Snape, and his eyes were full of tears."}
+	];
+	
+	
+}]);
